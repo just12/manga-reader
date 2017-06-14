44655cfd54da74731c35a79134e3f76244515f5d
just12 <jsjoseserrano9@gmail.com>
2017-06-14 18:49:17 +0200

    Create script.js

--- img adjust/script.js
+++ img adjust/script.js
@@ -1,16 +1,19 @@
+/*
 var img = document.getElementById("asadasa");
 var imgHeight = img.naturalHeight;
 var imgWidth = img.naturalWidth;
 var imgRatio = imgHeight / imgWidth;
 
-$(document).ready(function(){
-  $("a#esedese").text(imgRatio);
+setTimeout(function(){
+  $("a#esedese").text(imgHeight + "/" + imgWidth + "/" + imgRatio);
   if(imgRatio < 1){$("#asadasa").addClass("fitHeight");}
   if(imgRatio > 1){$("#asadasa").addClass("fitWidth");}
   if(imgRatio === 1){$("#asadasa").addClass("fitDefault");}
-});
+}, 1000);
 
-$(document).ready(function(){
+setTimeout(function(){
   $(".fitWidth").css("width", imgWidth + "px");
   $(".fitHeight").css("height", imgHeight + "px");
-});
+}, 1000);
+*/
+
